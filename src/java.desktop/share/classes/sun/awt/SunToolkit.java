73d7e69ce67f9f446c5eb9daec6a23d472881fc3
Alexey Ushakov <alexey.ushakov@jetbrains.com>
2023-12-19 22:04:59 +0100

    JBR-6445 Prepare RepaintManager code for displaySync=false
    Refactored AWTAccessor code

--- src/java.desktop/share/classes/sun/awt/SunToolkit.java
+++ src/java.desktop/share/classes/sun/awt/SunToolkit.java
@@ -2007,6 +2007,9 @@ public abstract class SunToolkit extends Toolkit
         return false;
     }
 
+    public boolean needUpdateWindowAfterPaint() {
+        return false;
+    }
     /**
      * Descendants of the SunToolkit should override and put their own logic here.
      */
