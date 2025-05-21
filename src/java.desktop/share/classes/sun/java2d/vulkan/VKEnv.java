JetBrains/JetBrainsRuntime

JBR-8410 Vulkan: Expose VKDevice on Java side

src/java.desktop/share/classes/sun/java2d/vulkan/VKEnv.java
@@ -30,50 +30,69 @@ import sun.util.logging.PlatformLogger;
 
 import java.security.AccessController;
 import java.security.PrivilegedAction;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class VKEnv {
 
     private static final PlatformLogger log = PlatformLogger.getLogger("sun.java2d.vulkan.VKInstance");
-    private static Boolean initialized;
+    private static Boolean enabled;
     private static Boolean sdAccelerated;
+    private static VKDevice[] devices;
+    private static VKDevice defaultDevice;
 
-    private static native boolean initNative(long nativePtr, boolean verbose, int deviceNumber);
+    private static native VKDevice[] initNative(long nativePtr, int deviceNumber);
 
     @SuppressWarnings({"removal", "restricted"})
     public static void init(long nativePtr) {
         String vulkanOption = AccessController.doPrivileged(
                         (PrivilegedAction<String>) () -> System.getProperty("sun.java2d.vulkan", ""));
         if ("true".equalsIgnoreCase(vulkanOption)) {
-            String deviceNumberOption = AccessController.doPrivileged(
-                    (PrivilegedAction<String>) () -> System.getProperty("sun.java2d.vulkan.deviceNumber", "0"));
-            int parsedDeviceNumber = 0;
-            try {
-                parsedDeviceNumber = Integer.parseInt(deviceNumberOption);
-            } catch (NumberFormatException e) {
-                log.warning("Invalid Vulkan device number:" + deviceNumberOption);
-            }
-            final int deviceNumber = parsedDeviceNumber;
-            final boolean verbose = "True".equals(vulkanOption);
             System.loadLibrary("awt");
             String sdOption = AccessController.doPrivileged(
                     (PrivilegedAction<String>) () -> System.getProperty("sun.java2d.vulkan.accelsd", ""));
-            initialized = initNative(nativePtr, verbose, deviceNumber);
-            sdAccelerated = initialized && "true".equalsIgnoreCase(sdOption);
-        } else initialized = false;
+            int deviceNumber = 0;
+            String deviceNumberOption = AccessController.doPrivileged(
+                    (PrivilegedAction<String>) () -> System.getProperty("sun.java2d.vulkan.deviceNumber"));
+            if (deviceNumberOption != null) {
+                try {
+                    deviceNumber = Integer.parseInt(deviceNumberOption);
+                } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+                    log.warning("Invalid Vulkan device number:" + deviceNumberOption);
+                }
+            }
+            devices = initNative(nativePtr, deviceNumber);
+            enabled = devices != null;
+            sdAccelerated = enabled && "true".equalsIgnoreCase(sdOption);
+            if (enabled) {
+                defaultDevice = devices[deviceNumber];
+                defaultDevice.getNativeHandle(); // Force init default device.
+            }
+        } else enabled = false;
 
         if (log.isLoggable(PlatformLogger.Level.FINE)) {
-            log.fine("Vulkan rendering enabled: " + (initialized ? "YES" : "NO"));
-            log.fine("Vulkan accelerated surface data enabled: " + (sdAccelerated ? "YES" : "NO"));
+            if (enabled) {
+                log.fine("Vulkan rendering enabled: YES" +
+                         "\n  accelerated surface data enabled: " + (sdAccelerated ? "YES" : "NO") +
+                         "\n  devices:" + Stream.of(devices).map(d -> (d == defaultDevice ?
+                         "\n    *" : "\n     ") + d.getName()).collect(Collectors.joining()));
+            } else log.fine("Vulkan rendering enabled: NO");
         }
     }
 
     public static boolean isVulkanEnabled() {
-        if (initialized == null) throw new RuntimeException("Vulkan not initialized");
-        return initialized;
+        if (enabled == null) throw new RuntimeException("Vulkan not initialized");
+        return enabled;
     }
 
     public static boolean isSurfaceDataAccelerated() {
-        if (initialized == null) throw new RuntimeException("Vulkan not initialized");
+        if (enabled == null) throw new RuntimeException("Vulkan not initialized");
         return sdAccelerated;
+    }
+
+    public static Stream<VKDevice> getDevices() {
+        if (enabled == null) throw new RuntimeException("Vulkan not initialized");
+        final VKDevice first = defaultDevice;
+        return Stream.concat(Stream.of(first), Stream.of(devices).filter(d -> d != first));
     }
 }

src/java.desktop/share/native/common/java2d/vulkan/VKDevice.c
@@ -170,6 +170,7 @@ void VKDevice_CheckAndAdd(VKEnv* vk, VkPhysicalDevice physicalDevice) {
 
     ARRAY_PUSH_BACK(vk->devices) = (VKDevice) {
         .name = deviceName,
+        .type = deviceProperties2.properties.deviceType,
         .handle = VK_NULL_HANDLE,
         .physicalDevice = physicalDevice,
         .queueFamily = queueFamily,
@@ -194,8 +195,34 @@ void VKDevice_Reset(VKDevice* device) {
     J2dRlsTraceLn1(J2D_TRACE_INFO, "VKDevice_Reset(%s)", device->name);
 }
 
-VkBool32 VKDevice_Init(JNIEnv *env, VKDevice* device) {
-    if (device->handle != VK_NULL_HANDLE) return VK_TRUE;
+/*
+ * Class:     sun_java2d_vulkan_VKDevice
+ * Method:    reset
+ * Signature: (J)void
+ */
+JNIEXPORT void JNICALL
+Java_sun_java2d_vulkan_VKDevice_reset(JNIEnv *env, jclass jClass, jlong jDevice) {
+    VKDevice* device = jlong_to_ptr(jDevice);
+    if (device == NULL) {
+        JNU_ThrowByName(env, "java/lang/IllegalStateException", "jDevice is NULL");
+        return;
+    }
+    VKDevice_Reset(device);
+}
+
+/*
+ * Class:     sun_java2d_vulkan_VKDevice
+ * Method:    init
+ * Signature: (J)void
+ */
+JNIEXPORT void JNICALL
+Java_sun_java2d_vulkan_VKDevice_init(JNIEnv *env, jclass jClass, jlong jDevice) {
+    VKDevice* device = jlong_to_ptr(jDevice);
+    if (device == NULL) {
+        JNU_ThrowByName(env, "java/lang/IllegalStateException", "jDevice is NULL");
+        return;
+    }
+    if (device->handle != VK_NULL_HANDLE) return;
 
     float queuePriority = 1.0f;
     VkDeviceQueueCreateInfo queueCreateInfo = {
@@ -228,7 +255,7 @@ VkBool32 VKDevice_Init(JNIEnv *env, VKDevice* device) {
     VKEnv* vk = VKEnv_GetInstance();
     VK_IF_ERROR(vk->vkCreateDevice(device->physicalDevice, &createInfo, NULL, &device->handle)) {
         JNU_ThrowByName(env, "java/lang/RuntimeException", "Cannot create device");
-        return VK_FALSE;
+        return;
     }
     J2dRlsTraceLn1(J2D_TRACE_INFO, "VKDevice_init(%s)", device->name);
 
@@ -240,37 +267,34 @@ VkBool32 VKDevice_Init(JNIEnv *env, VKDevice* device) {
         strcpy(message, fixedMessage);
         strcat(message, missingAPI);
         JNU_ThrowByName(env, "java/lang/RuntimeException", message);
-        return VK_FALSE;
+        return;
     }
 
     device->vkGetDeviceQueue(device->handle, device->queueFamily, 0, &device->queue);
     if (device->queue == NULL) {
         VKDevice_Reset(device);
         JNU_ThrowByName(env, "java/lang/RuntimeException", "Vulkan: Failed to get device queue");
-        return VK_FALSE;
+        return;
     }
 
     device->allocator = VKAllocator_Create(device);
     if (!device->allocator) {
         VKDevice_Reset(device);
         JNU_ThrowByName(env, "java/lang/RuntimeException", "Vulkan: Cannot create allocator");
-        return VK_FALSE;
+        return;
     }
 
     device->renderer = VKRenderer_Create(device);
     if (!device->renderer) {
         VKDevice_Reset(device);
         JNU_ThrowByName(env, "java/lang/RuntimeException", "Vulkan: Cannot create renderer");
-        return VK_FALSE;
+        return;
     }
 
     device->texturePool = VKTexturePool_InitWithDevice(device);
     if (!device->texturePool) {
         VKDevice_Reset(device);
         JNU_ThrowByName(env, "java/lang/RuntimeException", "Vulkan: Cannot create texture pool");
-        return VK_FALSE;
+        return;
     }
-
-    vk->currentDevice = device;
-    return VK_TRUE;
 }

src/java.desktop/share/native/common/java2d/vulkan/VKEnv.c
@@ -332,32 +332,48 @@ static VkBool32 VKEnv_FindDevices(VKEnv* vk) {
     return JNI_TRUE;
 }
 
+static jobjectArray createJavaDevices(JNIEnv *env, VKEnv* vk) {
+    jclass deviceClass = (*env)->FindClass(env, "sun/java2d/vulkan/VKDevice");
+    if (deviceClass == NULL) return NULL;
+    jmethodID deviceConstructor = (*env)->GetMethodID(env, deviceClass, "<init>", "(JLjava/lang/String;I)V");
+    if (deviceConstructor == NULL) return NULL;
+    jobjectArray deviceArray = (*env)->NewObjectArray(env, ARRAY_SIZE(vk->devices), deviceClass, NULL);
+    if (deviceArray == NULL) return NULL;
+    for (uint32_t i = 0; i < ARRAY_SIZE(vk->devices); i++) {
+        jstring name = JNU_NewStringPlatform(env, vk->devices[i].name);
+        if (name == NULL) return NULL;
+        jobject device = (*env)->NewObject(env, deviceClass, deviceConstructor,
+                                           ptr_to_jlong(&vk->devices[i]), name, vk->devices[i].type);
+        if (device == NULL) return NULL;
+        (*env)->SetObjectArrayElement(env, deviceArray, i, device);
+    }
+    return deviceArray;
+}
+
 static VKEnv* instance = NULL;
 VKEnv* VKEnv_GetInstance() {
     return instance;
 }
 
-VkBool32 VKDevice_Init(JNIEnv *env, VKDevice* device);
-
 /*
  * Class:     sun_java2d_vulkan_VKEnv
  * Method:    initNative
- * Signature: (JZI)Z
+ * Signature: (JI)[Lsun/java2d/vulkan/VKDevice;
  */
-JNIEXPORT jboolean JNICALL
-Java_sun_java2d_vulkan_VKEnv_initNative(JNIEnv *env, jclass wlge, jlong nativePtr, jboolean verb, jint requestedDevice) {
+JNIEXPORT jobjectArray JNICALL
+Java_sun_java2d_vulkan_VKEnv_initNative(JNIEnv *env, jclass wlge, jlong nativePtr, jint requestedDevice) {
 #ifdef DEBUG
     // Init random for debug-related validation tricks.
     srand(nativePtr);
 #endif
 
     PFN_vkGetInstanceProcAddr vkGetInstanceProcAddr = vulkanLibOpen();
-    if (vkGetInstanceProcAddr == NULL) return JNI_FALSE;
+    if (vkGetInstanceProcAddr == NULL) return NULL;
 
     VKEnv* vk = VKEnv_Create(vkGetInstanceProcAddr);
     if (vk == NULL) {
         vulkanLibClose();
-        return JNI_FALSE;
+        return NULL;
     }
 #if defined(VK_USE_PLATFORM_WAYLAND_KHR)
     vk->waylandDisplay = (struct wl_display*) jlong_to_ptr(nativePtr);
@@ -366,16 +382,19 @@ Java_sun_java2d_vulkan_VKEnv_initNative(JNIEnv *env, jclass wlge, jlong nativePt
     if (!VKEnv_FindDevices(vk)) {
         VKEnv_Destroy(vk);
         vulkanLibClose();
-        return JNI_FALSE;
+        return NULL;
     }
 
+    jobjectArray deviceArray = createJavaDevices(env, vk);
+    if (deviceArray == NULL) {
+        VKEnv_Destroy(vk);
+        vulkanLibClose();
+        return NULL;
+    }
+
+    vk->currentDevice = &vk->devices[requestedDevice];
     instance = vk;
-
-    if (requestedDevice < 0) requestedDevice = 0;
-    else if (requestedDevice >= (jint) ARRAY_SIZE(vk->devices)) requestedDevice = (jint) ARRAY_SIZE(vk->devices) - 1;
-    VKDevice_Init(env, &vk->devices[requestedDevice]);
-
-    return JNI_TRUE;
+    return deviceArray;
 }
 
 JNIEXPORT void JNICALL JNI_OnUnload(__attribute__((unused)) JavaVM *vm, __attribute__((unused)) void *reserved) {

openjdk/wakefield

src/java.desktop/share/classes/sun/java2d/vulkan/VKEnv.java
@@ -30,50 +30,69 @@ import sun.util.logging.PlatformLogger;
 
 import java.security.AccessController;
 import java.security.PrivilegedAction;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class VKEnv {
 
     private static final PlatformLogger log = PlatformLogger.getLogger("sun.java2d.vulkan.VKInstance");
-    private static Boolean initialized;
+    private static Boolean enabled;
     private static Boolean sdAccelerated;
+    private static VKGPU[] devices;
+    private static VKGPU defaultDevice;
 
-    private static native boolean initNative(long nativePtr, boolean verbose, int deviceNumber);
+    private static native VKGPU[] initNative(long nativePtr, int deviceNumber);
 
     @SuppressWarnings({"removal", "restricted"})
     public static void init(long nativePtr) {
         String vulkanOption = AccessController.doPrivileged(
                         (PrivilegedAction<String>) () -> System.getProperty("sun.java2d.vulkan", ""));
         if ("true".equalsIgnoreCase(vulkanOption)) {
-            String deviceNumberOption = AccessController.doPrivileged(
-                    (PrivilegedAction<String>) () -> System.getProperty("sun.java2d.vulkan.deviceNumber", "0"));
-            int parsedDeviceNumber = 0;
-            try {
-                parsedDeviceNumber = Integer.parseInt(deviceNumberOption);
-            } catch (NumberFormatException e) {
-                log.warning("Invalid Vulkan device number:" + deviceNumberOption);
-            }
-            final int deviceNumber = parsedDeviceNumber;
-            final boolean verbose = "True".equals(vulkanOption);
             System.loadLibrary("awt");
             String sdOption = AccessController.doPrivileged(
                     (PrivilegedAction<String>) () -> System.getProperty("sun.java2d.vulkan.accelsd", ""));
-            initialized = initNative(nativePtr, verbose, deviceNumber);
-            sdAccelerated = initialized && "true".equalsIgnoreCase(sdOption);
-        } else initialized = false;
+            int deviceNumber = 0;
+            String deviceNumberOption = AccessController.doPrivileged(
+                    (PrivilegedAction<String>) () -> System.getProperty("sun.java2d.vulkan.deviceNumber"));
+            if (deviceNumberOption != null) {
+                try {
+                    deviceNumber = Integer.parseInt(deviceNumberOption);
+                } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+                    log.warning("Invalid Vulkan device number:" + deviceNumberOption);
+                }
+            }
+            devices = initNative(nativePtr, deviceNumber);
+            enabled = devices != null;
+            sdAccelerated = enabled && "true".equalsIgnoreCase(sdOption);
+            if (enabled) {
+                defaultDevice = devices[deviceNumber];
+                defaultDevice.getNativeHandle(); // Force init default device.
+            }
+        } else enabled = false;
 
         if (log.isLoggable(PlatformLogger.Level.FINE)) {
-            log.fine("Vulkan rendering enabled: " + (initialized ? "YES" : "NO"));
-            log.fine("Vulkan accelerated surface data enabled: " + (sdAccelerated ? "YES" : "NO"));
+            if (enabled) {
+                log.fine("Vulkan rendering enabled: YES" +
+                         "\n  accelerated surface data enabled: " + (sdAccelerated ? "YES" : "NO") +
+                         "\n  devices:" + Stream.of(devices).map(d -> (d == defaultDevice ?
+                         "\n    *" : "\n     ") + d.getName()).collect(Collectors.joining()));
+            } else log.fine("Vulkan rendering enabled: NO");
         }
     }
 
     public static boolean isVulkanEnabled() {
-        if (initialized == null) throw new RuntimeException("Vulkan not initialized");
-        return initialized;
+        if (enabled == null) throw new RuntimeException("Vulkan not initialized");
+        return enabled;
     }
 
     public static boolean isSurfaceDataAccelerated() {
-        if (initialized == null) throw new RuntimeException("Vulkan not initialized");
+        if (enabled == null) throw new RuntimeException("Vulkan not initialized");
         return sdAccelerated;
+    }
+
+    public static Stream<VKGPU> getDevices() {
+        if (enabled == null) throw new RuntimeException("Vulkan not initialized");
+        final VKGPU first = defaultDevice;
+        return Stream.concat(Stream.of(first), Stream.of(devices).filter(d -> d != first));
     }
 }

src/java.desktop/share/native/common/java2d/vulkan/VKDevice.c
@@ -170,6 +170,7 @@ void VKDevice_CheckAndAdd(VKEnv* vk, VkPhysicalDevice physicalDevice) {
 
     ARRAY_PUSH_BACK(vk->devices) = (VKDevice) {
         .name = deviceName,
+        .type = deviceProperties2.properties.deviceType,
         .handle = VK_NULL_HANDLE,
         .physicalDevice = physicalDevice,
         .queueFamily = queueFamily,
@@ -194,8 +195,34 @@ void VKDevice_Reset(VKDevice* device) {
     J2dRlsTraceLn1(J2D_TRACE_INFO, "VKDevice_Reset(%s)", device->name);
 }
 
-VkBool32 VKDevice_Init(JNIEnv *env, VKDevice* device) {
-    if (device->handle != VK_NULL_HANDLE) return VK_TRUE;
+/*
+ * Class:     sun_java2d_vulkan_VKGPU
+ * Method:    reset
+ * Signature: (J)void
+ */
+JNIEXPORT void JNICALL
+Java_sun_java2d_vulkan_VKGPU_reset(JNIEnv *env, jclass jClass, jlong jDevice) {
+    VKDevice* device = jlong_to_ptr(jDevice);
+    if (device == NULL) {
+        JNU_ThrowByName(env, "java/lang/IllegalStateException", "jDevice is NULL");
+        return;
+    }
+    VKDevice_Reset(device);
+}
+
+/*
+ * Class:     sun_java2d_vulkan_VKGPU
+ * Method:    init
+ * Signature: (J)void
+ */
+JNIEXPORT void JNICALL
+Java_sun_java2d_vulkan_VKGPU_init(JNIEnv *env, jclass jClass, jlong jDevice) {
+    VKDevice* device = jlong_to_ptr(jDevice);
+    if (device == NULL) {
+        JNU_ThrowByName(env, "java/lang/IllegalStateException", "jDevice is NULL");
+        return;
+    }
+    if (device->handle != VK_NULL_HANDLE) return;
 
     float queuePriority = 1.0f;
     VkDeviceQueueCreateInfo queueCreateInfo = {
@@ -228,7 +255,7 @@ VkBool32 VKDevice_Init(JNIEnv *env, VKDevice* device) {
     VKEnv* vk = VKEnv_GetInstance();
     VK_IF_ERROR(vk->vkCreateDevice(device->physicalDevice, &createInfo, NULL, &device->handle)) {
         JNU_ThrowByName(env, "java/lang/RuntimeException", "Cannot create device");
-        return VK_FALSE;
+        return;
     }
     J2dRlsTraceLn1(J2D_TRACE_INFO, "VKDevice_init(%s)", device->name);
 
@@ -240,37 +267,34 @@ VkBool32 VKDevice_Init(JNIEnv *env, VKDevice* device) {
         strcpy(message, fixedMessage);
         strcat(message, missingAPI);
         JNU_ThrowByName(env, "java/lang/RuntimeException", message);
-        return VK_FALSE;
+        return;
     }
 
     device->vkGetDeviceQueue(device->handle, device->queueFamily, 0, &device->queue);
     if (device->queue == NULL) {
         VKDevice_Reset(device);
         JNU_ThrowByName(env, "java/lang/RuntimeException", "Vulkan: Failed to get device queue");
-        return VK_FALSE;
+        return;
     }
 
     device->allocator = VKAllocator_Create(device);
     if (!device->allocator) {
         VKDevice_Reset(device);
         JNU_ThrowByName(env, "java/lang/RuntimeException", "Vulkan: Cannot create allocator");
-        return VK_FALSE;
+        return;
     }
 
     device->renderer = VKRenderer_Create(device);
     if (!device->renderer) {
         VKDevice_Reset(device);
         JNU_ThrowByName(env, "java/lang/RuntimeException", "Vulkan: Cannot create renderer");
-        return VK_FALSE;
+        return;
     }
 
     device->texturePool = VKTexturePool_InitWithDevice(device);
     if (!device->texturePool) {
         VKDevice_Reset(device);
         JNU_ThrowByName(env, "java/lang/RuntimeException", "Vulkan: Cannot create texture pool");
-        return VK_FALSE;
+        return;
     }
-
-    vk->currentDevice = device;
-    return VK_TRUE;
 }

src/java.desktop/share/native/common/java2d/vulkan/VKEnv.c
@@ -332,32 +332,48 @@ static VkBool32 VKEnv_FindDevices(VKEnv* vk) {
     return JNI_TRUE;
 }
 
+static jobjectArray createJavaGPUs(JNIEnv *env, VKEnv* vk) {
+    jclass deviceClass = (*env)->FindClass(env, "sun/java2d/vulkan/VKGPU");
+    if (deviceClass == NULL) return NULL;
+    jmethodID deviceConstructor = (*env)->GetMethodID(env, deviceClass, "<init>", "(JLjava/lang/String;I)V");
+    if (deviceConstructor == NULL) return NULL;
+    jobjectArray deviceArray = (*env)->NewObjectArray(env, ARRAY_SIZE(vk->devices), deviceClass, NULL);
+    if (deviceArray == NULL) return NULL;
+    for (uint32_t i = 0; i < ARRAY_SIZE(vk->devices); i++) {
+        jstring name = JNU_NewStringPlatform(env, vk->devices[i].name);
+        if (name == NULL) return NULL;
+        jobject device = (*env)->NewObject(env, deviceClass, deviceConstructor,
+                                           ptr_to_jlong(&vk->devices[i]), name, vk->devices[i].type);
+        if (device == NULL) return NULL;
+        (*env)->SetObjectArrayElement(env, deviceArray, i, device);
+    }
+    return deviceArray;
+}
+
 static VKEnv* instance = NULL;
 VKEnv* VKEnv_GetInstance() {
     return instance;
 }
 
-VkBool32 VKDevice_Init(JNIEnv *env, VKDevice* device);
-
 /*
  * Class:     sun_java2d_vulkan_VKEnv
  * Method:    initNative
- * Signature: (JZI)Z
+ * Signature: (JI)[Lsun/java2d/vulkan/VKDevice;
  */
-JNIEXPORT jboolean JNICALL
-Java_sun_java2d_vulkan_VKEnv_initNative(JNIEnv *env, jclass wlge, jlong nativePtr, jboolean verb, jint requestedDevice) {
+JNIEXPORT jobjectArray JNICALL
+Java_sun_java2d_vulkan_VKEnv_initNative(JNIEnv *env, jclass wlge, jlong nativePtr, jint requestedDevice) {
 #ifdef DEBUG
     // Init random for debug-related validation tricks.
     srand(nativePtr);
 #endif
 
     PFN_vkGetInstanceProcAddr vkGetInstanceProcAddr = vulkanLibOpen();
-    if (vkGetInstanceProcAddr == NULL) return JNI_FALSE;
+    if (vkGetInstanceProcAddr == NULL) return NULL;
 
     VKEnv* vk = VKEnv_Create(vkGetInstanceProcAddr);
     if (vk == NULL) {
         vulkanLibClose();
-        return JNI_FALSE;
+        return NULL;
     }
 #if defined(VK_USE_PLATFORM_WAYLAND_KHR)
     vk->waylandDisplay = (struct wl_display*) jlong_to_ptr(nativePtr);
@@ -366,16 +382,19 @@ Java_sun_java2d_vulkan_VKEnv_initNative(JNIEnv *env, jclass wlge, jlong nativePt
     if (!VKEnv_FindDevices(vk)) {
         VKEnv_Destroy(vk);
         vulkanLibClose();
-        return JNI_FALSE;
+        return NULL;
     }
 
+    jobjectArray deviceArray = createJavaGPUs(env, vk);
+    if (deviceArray == NULL) {
+        VKEnv_Destroy(vk);
+        vulkanLibClose();
+        return NULL;
+    }
+
+    vk->currentDevice = &vk->devices[requestedDevice];
     instance = vk;
-
-    if (requestedDevice < 0) requestedDevice = 0;
-    else if (requestedDevice >= (jint) ARRAY_SIZE(vk->devices)) requestedDevice = (jint) ARRAY_SIZE(vk->devices) - 1;
-    VKDevice_Init(env, &vk->devices[requestedDevice]);
-
-    return JNI_TRUE;
+    return deviceArray;
 }
 
 JNIEXPORT void JNICALL JNI_OnUnload(__attribute__((unused)) JavaVM *vm, __attribute__((unused)) void *reserved) {