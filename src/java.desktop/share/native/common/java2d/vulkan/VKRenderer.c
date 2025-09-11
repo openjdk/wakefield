c93089194b4269a3218c42e123407e30793cf45b
Maxim Kartashev <maxim.kartashev@jetbrains.com>
2025-09-11 15:20:37 +0400

    Revert "JBR-8740 Vulkan: Optimize BLIT"
    This reverts commit f717596c31a44955c1d4d7858f2ec8ce763d69fa.

--- src/java.desktop/share/native/common/java2d/vulkan/VKRenderer.c
+++ src/java.desktop/share/native/common/java2d/vulkan/VKRenderer.c
@@ -48,6 +48,10 @@ VKRenderingContext* VKRenderer_GetContext() {
     return &context;
 }
 
+void VKRenderer_DisposeOnPrimaryComplete(VKRenderer* renderer, VKCleanupHandler handler, void* data) {
+    POOL_RETURN(renderer, renderer->cleanupQueue, ((VKCleanupEntry) { handler, data }));
+}
+
 static void VKRenderer_CleanupPendingResources(VKRenderer* renderer) {
     VKDevice* device = renderer->device;
     for (VKCleanupEntry entry; POOL_TAKE(renderer, renderer->cleanupQueue, entry);) entry.handler(device, entry.data);
