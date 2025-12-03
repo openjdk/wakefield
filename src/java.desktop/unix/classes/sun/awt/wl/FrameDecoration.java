JetBrains/JetBrainsRuntime

JBR-6187 Wayland: implement server-side decoration support
Use -Dsun.awt.wl.WindowDecorationStyle=server to activate

src/java.desktop/unix/classes/sun/awt/wl/FrameDecoration.java
@@ -130,5 +130,7 @@ public abstract class FrameDecoration {
         return null;
     }
 
+    public abstract void nativeWindowCreated(long nativePtr);
+    public abstract void nativeWindowToBeHidden(long nativePtr);
     public abstract void dispose();
 }

src/java.desktop/unix/classes/sun/awt/wl/FullFrameDecorationHelper.java
@@ -295,6 +295,14 @@ public abstract class FullFrameDecorationHelper extends FrameDecoration {
     }
 
     @Override
+    public void nativeWindowCreated(long nativePtr) {
+    }
+
+    @Override
+    public void nativeWindowToBeHidden(long nativePtr) {
+    }
+
+    @Override
     public void dispose() {
         WLToolkit.getDefaultToolkit().removePropertyChangeListener("awt.os.theme.isDark", pcl);
     }

openjdk/wakefield

src/java.desktop/unix/classes/sun/awt/wl/FrameDecoration.java
@@ -26,7 +26,6 @@ package sun.awt.wl;
 
 import java.awt.Cursor;
 import java.awt.Dimension;
-import java.awt.Frame;
 import java.awt.Graphics;
 import java.awt.Insets;
 import java.awt.Point;
@@ -130,5 +129,7 @@ public abstract class FrameDecoration {
         return null;
     }
 
+    public abstract void notifyNativeWindowCreated(long nativePtr);
+    public abstract void notifyNativeWindowToBeHidden(long nativePtr);
     public abstract void dispose();
 }

src/java.desktop/unix/classes/sun/awt/wl/FullFrameDecorationHelper.java
@@ -295,6 +295,14 @@ public abstract class FullFrameDecorationHelper extends FrameDecoration {
     }
 
     @Override
+    public void notifyNativeWindowCreated(long nativePtr) {
+    }
+
+    @Override
+    public void notifyNativeWindowToBeHidden(long nativePtr) {
+    }
+
+    @Override
     public void dispose() {
         WLToolkit.getDefaultToolkit().removePropertyChangeListener("awt.os.theme.isDark", pcl);
     }